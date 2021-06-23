c025bc5af71b49ceb5ff759a77d85af299021c4e
Cole Willsea <cowills@rei.com>
2021-06-23 09:33:54 -0700

    clean up docs/readme

---
+++ templates/[name].vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style[baseClass]">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: '{NAME-FULLNAME}',
+  setup(props) {
+    const baseClass = '{NAME-TAGNAME}';
+    return {
+      baseClass,
+    };
+  },
+});
+</script>
+
+<style lang="scss" module src="./styles/{NAME-FULLNAME}.scss">
+</style>
