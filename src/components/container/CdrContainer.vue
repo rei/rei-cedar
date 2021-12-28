df0b2a556238db69b8f0d826f19605a0ef2dc830
Ben Griffin <bgriffi@rei.com>
2021-12-28 10:53:49 -0800

    refactor(container): adds script setup syntac, removes buildClass usage

--- src/components/container/CdrContainer.vue
+++ src/components/container/CdrContainer.vue
@@ -1,45 +1,38 @@
+<script setup>
+import { useCssModule, computed } from 'vue';
+import propValidator from '../../utils/propValidator';
+import mapClasses from '../../utils/mapClasses';
+
+const props = defineProps({
+  /** Any valid HTML tag */
+  tag: {
+    type: String,
+    default: 'div',
+  },
+  modifier: {
+    type: String,
+    default: 'static',
+    validator: (value) => propValidator(
+      value,
+      ['static', 'fluid'],
+      false,
+    ),
+  },
+});
+
+const baseClass = 'cdr-container';
+const style = useCssModule();
+
+/************************  Computed properties *************************/
+const modifierClass = computed(() => `${baseClass}--${props.modifier}`);
+
+</script>
+
 <template>
   <component :is="tag" :class="mapClasses(style, baseClass, modifierClass)">
     <slot />
   </component>
 </template>
-
-<script>
-import { defineComponent, useCssModule, computed } from 'vue';
-import propValidator from '../../utils/propValidator';
-import { buildClass } from '../../utils/buildClass';
-import mapClasses from '../../utils/mapClasses';
-
-export default defineComponent({
-  name: 'CdrContainer',
-  props: {
-    /** Any valid HTML tag */
-    tag: {
-      type: String,
-      default: 'div',
-    },
-    modifier: {
-      type: String,
-      default: 'static',
-      validator: (value) => propValidator(
-        value,
-        ['static', 'fluid'],
-        false,
-      ),
-    },
-  },
-  setup(props, ctx) {
-    const baseClass = 'cdr-container';
-    const modifierClass = computed(() => buildClass(baseClass, props.modifier));
-    return {
-      style: useCssModule(),
-      baseClass,
-      modifierClass,
-      mapClasses,
-    };
-  },
-});
-</script>
 
 <style lang="scss" module src="./styles/CdrContainer.module.scss">
 </style>
