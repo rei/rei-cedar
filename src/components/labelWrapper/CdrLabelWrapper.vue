732d181828f56d0eeddd0ce198e3338f91274948
Kenji Crosland <kcrosla@rei.com>
2022-02-02 09:35:17 -0600

    Update cdr-labelWrapper for <script setup> syntax. Fix CDR-2349 accessibility issue

--- src/components/labelWrapper/CdrLabelWrapper.vue
+++ src/components/labelWrapper/CdrLabelWrapper.vue
@@ -11,43 +11,31 @@
     >
       <slot name="input" />
       <span :class="style['cdr-label-wrapper__figure']" />
-      <div :class="mapClasses(style, 'cdr-label-wrapper__content', contentClass)">
+      <span :class="mapClasses(style, 'cdr-label-wrapper__content', contentClass)">
         <slot />
-      </div>
+      </span>
     </label>
   </div>
 </template>
-<script>
-import { defineComponent, useCssModule, computed } from 'vue';
-import { responsiveModifyClass } from '../../utils/buildClass';
-import backgroundProps from '../../props/background';
-import mapClasses from '../../utils/mapClasses';
+<script setup>
+  import { useCssModule, computed } from 'vue';
+  import { responsiveModifyClass } from '../../utils/buildClass';
+  import backgroundProps from '../../props/background';
+  import mapClasses from '../../utils/mapClasses';
 
-export default defineComponent({
-  name: 'CdrLabelWrapper',
-  props: {
-    labelClass: String,
-    contentClass: String,
-    background: backgroundProps,
-    disabled: Boolean,
-    modifier: String,
-    size: String,
-  },
-  setup(props) {
-    const baseClass = 'cdr-label-wrapper';
-    const modifierClass = computed(() => props.modifier && `${baseClass}--${props.modifier}`);
-    const sizeClass = computed(() => props.size && responsiveModifyClass(baseClass, '', props.size));
-    const disabledClass = computed(() => props.disabled && `${baseClass}--disabled`);
-    return {
-      style: useCssModule(),
-      mapClasses,
-      baseClass,
-      modifierClass,
-      sizeClass,
-      disabledClass,
-    };
-  },
-});
+  const props = defineProps({
+      labelClass: String,
+      contentClass: String,
+      background: backgroundProps,
+      disabled: Boolean,
+      modifier: String,
+      size: String,
+  })
+  const style = useCssModule();
+  const baseClass = 'cdr-label-wrapper';
+  const modifierClass = computed(() => props.modifier && `${baseClass}--${props.modifier}`);
+  const sizeClass = computed(() => props.size && responsiveModifyClass(baseClass, '', props.size));
+  const disabledClass = computed(() => props.disabled && `${baseClass}--disabled`);
 </script>
 
 <style lang="scss" module src="./styles/CdrLabelWrapper.module.scss">
