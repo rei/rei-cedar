9199941fd6f592514758ed93d60a34fab74eb631
Kenji Crosland <kcrosla@rei.com>
2022-04-14 14:50:10 -0400

    Style fixes

--- src/components/tabs/CdrTabs.vue
+++ src/components/tabs/CdrTabs.vue
@@ -3,7 +3,19 @@
     :class="mapClasses(style, baseClass, modifierClass, sizeClass)"
     :style="{ height }"
   >
-    <ul :class="style['cdr-tabs__header-container']" role="tablist">
+    <div
+      :class="style['cdr-tabs__gradient-container']"
+    >
+      <div
+        :class="mapClasses(
+          style,
+          'cdr-tabs__gradient',
+          'cdr-tabs__gradient--left',
+          overflowLeft ? 'cdr-tabs__gradient--active' : ''
+        )"
+        :style="gradientLeftStyle"
+      />
+    <ul :class="style['cdr-tabs__header-container']" role="tablist" ref="tablist">
       <li v-for="(tab, index) in tabs" 
         :key="tab.name" role="presentation"
         :class="style['cdr-tabs__header']" 
@@ -28,6 +40,20 @@
         >{{ tab.name }}</button>
       </li>
     </ul>
+    <div
+        :class="mapClasses(
+          style,
+          'cdr-tabs__gradient',
+          'cdr-tabs__gradient--right',
+          overflowRight ? 'cdr-tabs__gradient--active' : '',
+        )"
+        :style="gradientRightStyle"
+      />
+      <div
+        :class="style['cdr-tabs__underline']"
+        :style="underlineStyle"
+      />
+    </div>
     <slot></slot>
   </div>
 </template>
@@ -58,15 +84,60 @@ const props = defineProps({
     }
 })
 
-const slots = useSlots()
-const tabs = ref(slots.default().map((tab) => ({ name: tab.props.name, disabled: tab.props.disabled })));
+const slots = useSlots();
+const slottedTabs = slots.default()[0].children.length ? slots.default()[0].children : slots.default();
+const baseClass = 'cdr-tabs';
+
+const tabs = ref(slottedTabs.map((tab) => ({ name: tab.props.name, disabled: tab.props.disabled })));
 const selectedTabName = ref(null);
 const selectedIndex = ref(null);
 const tabElements = ref([]);
-const baseClass = 'cdr-tabs';
+const tablist = ref(null);
+const overflowLeft = ref(false);
+const overflowRight = ref(false);
+const underlineOffsetX = ref(0);
+const underlineWidth = ref(0);
 
 const modifierClass = computed(() => buildClass('cdr-tabs', props.modifier));
 const sizeClass = computed(() => props.size && buildClass('cdr-tabs', props.size));
+const underlineStyle = computed(() => ({
+    transform: `translateX(${underlineOffsetX.value}px)`,
+    width: `${underlineWidth.value}px`,
+}));
+const gradientLeftStyle = computed(() => {
+    const gradient = `linear-gradient(to left, rgba(255, 255, 255, 0), ${props.backgroundColor})`;
+    return {
+    background: gradient,
+    };
+});
+const gradientRightStyle = computed(() => {
+    const gradient = `linear-gradient(to right, rgba(255, 255, 255, 0), ${props.backgroundColor})`;
+    return {
+    background: gradient,
+    };
+});
+
+const updateUnderline = () => {
+    if (tabElements.value.length > 0) {
+    const activeTab = tabElements.value[selectedIndex.value];
+    const activeRect = activeTab.getBoundingClientRect();
+    const parentRect = tablist.value.getBoundingClientRect();
+    const offset = activeRect.x - parentRect.x;
+
+    underlineOffsetX.value = offset
+        - tablist.value.parentElement.scrollLeft;
+    underlineWidth.value = activeRect.width;
+
+    // shrink/hide the underline if it scrolls outside the container
+    if (underlineOffsetX.value + underlineWidth.value >= parentRect.width) {
+        underlineWidth.value = Math.max(0, parentRect.width - underlineOffsetX.value);
+        underlineOffsetX.value = Math.min(underlineOffsetX.value, parentRect.width);
+    } else if (underlineOffsetX.value < 0) {
+        underlineWidth.value = Math.max(0, underlineWidth.value + underlineOffsetX.value);
+        underlineOffsetX.value = 0;
+    }
+    }
+};
 
 provide("selectedTabName", selectedTabName);
 
@@ -117,19 +188,24 @@ const selectTab = async (index) => {
   selectedIndex.value = index;
   await nextTick();
   tabToSelect.focus();
+  updateUnderline();
 }
 
 onMounted(() => {
   setInitialTabStates();
+  setTimeout(()=>{
+    updateUnderline();
+  }, 250);
 })
 
 const setInitialTabStates = () => {
-tabElements.value.forEach((tab, index) =>{
-    if(!tab.disabled && selectedIndex.value === null){
-        selectedIndex.value = index;
-        selectedTabName.value = tabs.value[index].name;
-    }
- })
+    console.log(tabElements.value)
+    tabElements.value.forEach((tab, index) =>{
+        if(!tab.disabled && selectedIndex.value === null){
+            selectedIndex.value = index;
+            selectedTabName.value = tabs.value[index].name;
+        }
+    })
 }
 
 const style = useCssModule();
