5a4a0259eb327068a762fe21015608c93210d714
Kenji Crosland <kcrosla@rei.com>
2022-05-03 15:36:48 -0500

    fix(cdr-select): remove role="status" default for errors. Add error-id to aria-describedby attr
    CDR-2355

--- src/components/select/CdrSelect.vue
+++ src/components/select/CdrSelect.vue
@@ -60,7 +60,7 @@ const props = defineProps({
   errorRole: {
     type: String,
     required: false,
-    default: 'status',
+    default: '',
   },
   modelValue: {
     type: [String, Number, Boolean, Object, Array, Symbol, Function],
@@ -90,10 +90,16 @@ const sizeClass = computed(() => props.size && `${baseClass}--${props.size}`);
 const caretDisabledClass = computed(() => props.disabled && 'cdr-select__caret--disabled');
 
 const describedby = computed(() => {
-  return [
+  const helperText =  [
     slots['helper-text'] ? `${uniqueId}-helper-text-top` : '',
     attrs['aria-describedby'],
   ].filter((x) => x).join(' ');
+
+  if(props.error) {
+    return `${uniqueId}-error`;
+  }
+
+  return helperText;
 })
 
 // TODO: refactor, would be much clearer as a 1-2 liner
