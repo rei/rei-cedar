417e41a0d235b15243dd3f92fed4762f77c93c73
Kenji Crosland <kcrosla@rei.com>
2022-05-19 14:36:01 -0500

    test(cdr-banner): refactor cdr-banner test
    CDR-2541

--- src/components/banner/__tests__/CdrBanner.spec.js
+++ src/components/banner/__tests__/CdrBanner.spec.js
@@ -1,45 +1,95 @@
+import { expect } from 'vitest';
 import { mount } from '../../../../test/vue-jest-style-workaround.js';
 import CdrBanner from '../CdrBanner.vue';
 
 describe('CdrBanner', () => {
-  it('renders correctly', () => {
-    const wrapper = mount(CdrBanner, {
-      slots: {
-        default: 'hey im a banner',
-        'icon-left': '<div />'
-      }
+  describe('with icon-left slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrBanner, {
+        slots: {
+          default: 'hey im a banner',
+          'icon-left': '<div />'
+        }
+      });
+    })
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.element).toMatchSnapshot();
   });
-  it('renders a right-icon', () => {
-    const wrapper = mount(CdrBanner, {
-      slots: {
-        default: 'hey im a banner',
-        'icon-left': '<div />',
-        'icon-right': '<div />',
-      }
+
+  describe('with icon-right slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrBanner, {
+        slots: {
+          default: 'hey im a banner',
+          'icon-left': '<div />',
+          'icon-right': '<div />',
+        }
+      });
+    })
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.element).toMatchSnapshot();
   });
-  it('renders the info-action slot', () => {
-    const wrapper = mount(CdrBanner, {
-      slots: {
-        default: 'hey im a banner',
-        'icon-left': '<div />',
-        'icon-right': '<div />',
-        'info-action': '<div />',
-      }
+
+  describe('with info-action slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrBanner, {
+        slots: {
+          default: 'hey im a banner',
+          'icon-left': '<div />',
+          'icon-right': '<div />',
+          'info-action': '<div />',
+        }
+      });
+    })
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.element).toMatchSnapshot();
   });
-  it('renders the message-body slot', () => {
-    const wrapper = mount(CdrBanner, {
-      slots: {
-        default: 'hey im a banner',
-        'icon-left': '<div />',
-        'message-body': 'and I am some extra information'
-      }
+
+  describe('with message-body slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrBanner, {
+        slots: {
+          default: 'hey im a banner',
+          'icon-left': '<div />',
+          'message-body': 'and I am some extra information'
+        }
+      });
+    })
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.element).toMatchSnapshot();
   });
+
+  describe('with just the default slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrBanner, {
+        slots: {
+          default: 'hey im a banner',
+        }
+      });
+    })
+    describe('with "type" prop set to "info"', () => {
+      beforeEach(() => {
+        wrapper.setProps({
+          type: 'info',
+        });
+      });
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      })
+
+      it('has the expected typeClass', () => {
+        expect(wrapper.vm.typeClass).toBe('cdr-banner--info');
+      });
+    })
+  })
 });
