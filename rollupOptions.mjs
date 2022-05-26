89ce1fc2e4d9cfd9b1181e77221bec2ed2aca4e7
Ben Griffin <grif88@gmail.com>
2022-05-26 12:02:27 -0700

    build(vite): add support for polyfills via babel/core-js

---
+++ rollupOptions.mjs
@@ -0,0 +1,34 @@
+import { babel } from '@rollup/plugin-babel';
+import browserTargets from './supportedBrowers.mjs';
+
+export default {
+  external: ['vue', /core-js/],
+  output: {
+    globals: {
+      vue: 'Vue'
+    }
+  },
+  plugins: [
+      babel({
+      exclude: ['node_modules/**'],
+      babelHelpers: 'bundled',
+      babelrc: false,
+      extensions: ['.js', '.mjs', '.vue'],
+      targets: browserTargets, // supported browsers.
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false, // don't convert ESM modules, as that breaks tree shaking
+            targets: browserTargets, // polyfill based on supported browsers
+            corejs: { // use corejs@3 for polyfills
+              version: '3.22', // see https://github.com/zloirock/core-js#babelpreset-env
+            },
+            useBuiltIns: 'usage', // polyfill based on usage in source code.
+            bugfixes: true, // See https://github.com/babel/babel/issues/13916 (note, we can remove this when Babel 8 is released.
+          },
+        ],
+      ],
+    })
+  ]
+}
