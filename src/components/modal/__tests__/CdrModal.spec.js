94ed3121946829e3796102cc4262a01a9ce5157a
Kenji Crosland <kcrosla@rei.com>
2022-06-13 15:06:15 -0500

    test(cdr-modal tests): refactor cedar modal tests

--- src/components/modal/__tests__/CdrModal.spec.js
+++ src/components/modal/__tests__/CdrModal.spec.js
@@ -2,109 +2,47 @@ import { mount } from '../../../../test/vue-jest-style-workaround.js';
 import CdrModal from '../CdrModal.vue';
 
 describe('CdrModal.vue', () => {
-  it('default open', async () => {
-    const elem = document.createElement('div')
-    if (document.body) {
-      document.body.appendChild(elem)
-    }
-    const wrapper = mount(CdrModal, {
-      propsData: {
-        opened: true,
-        label: "Label is the modal title"
-      },
-      slots: {
-        default: 'Sticky content',
-      },
-      attachTo: elem,
+  describe('default open', ()=>{
+    let wrapper;
+    let elem;
+    beforeEach(()=>{
+      elem = document.createElement('div')
+      if (document.body) {
+        document.body.appendChild(elem)
+      }
+      wrapper = mount(CdrModal, {
+        propsData: {
+          opened: true,
+          label: "Label is the modal title"
+        },
+        slots: {
+          default: 'Sticky content',
+        },
+        attachTo: elem,
+      });
     });
 
-    await wrapper.vm.$nextTick();
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
 
-    expect(wrapper.element).toMatchSnapshot();
-  });
+    it('handleKeyDown', async () => {
+      wrapper.trigger('keydown', {
+        key: 'a'
+      });
+      await wrapper.vm.$nextTick();
   
-  it('handleKeyDown', async () => {
-    const elem = document.createElement('div')
-    if (document.body) {
-      document.body.appendChild(elem)
-    }
-    const wrapper = mount(CdrModal, {
-      propsData: {
-        opened: true,
-        label: "Label is the modal title"
-      },
-      slots: {
-        default: 'Main content',
-      },
-      attachTo: elem,
+      wrapper.trigger('keydown', {
+        key: 'Esc',
+      });
+      await wrapper.vm.$nextTick();
+  
+      wrapper.trigger('keydown', {
+        key: 'Escape',
+      });
+      await wrapper.vm.$nextTick();
+  
+      expect(wrapper.emitted().closed.length).toBe(2);
     });
-
-    wrapper.trigger('keydown', {
-      key: 'a'
-    });
-    await wrapper.vm.$nextTick();
-
-    wrapper.trigger('keydown', {
-      key: 'Esc',
-    });
-    await wrapper.vm.$nextTick();
-
-    wrapper.trigger('keydown', {
-      key: 'Escape',
-    });
-    await wrapper.vm.$nextTick();
-
-    expect(wrapper.emitted().closed.length).toBe(2);
   });
-
-  // it('fullscreen snapshot', async () => {
-  //   const elem = document.createElement('div')
-  //   if (document.body) {
-  //     document.body.appendChild(elem)
-  //   }
-  //   const wrapper = mount(CdrModal, {
-  //     propsData: {
-  //       opened: true,
-  //       label: "Label is the modal title"
-  //     },
-  //     data() {
-  //       return {
-  //         fullscreen: true,
-  //       };
-  //     },
-  //     slots: {
-  //       scrollingContentSlot: 'Main content',
-  //     },
-  //     attachTo: elem,
-  //   });
-
-
-  //   await wrapper.vm.$nextTick();
-
-  //   expect(wrapper.element).toMatchSnapshot();
-  // });
-
-  // it('removeNoScroll', async () => {
-  //   const elem = document.createElement('div')
-  //   if (document.body) {
-  //     document.body.appendChild(elem)
-  //   }
-  //   const wrapper = mount(CdrModal, {
-  //     propsData: {
-  //       opened: true,
-  //       label: "My Modal Label",
-  //     },
-  //     slots: {
-  //       scrollingContentSlot: 'Main content',
-  //     },
-  //     attachTo: elem,
-  //   });
-  //   const { documentElement, body } = document;
-  //   expect(documentElement.classList.contains('cdr-modal__noscroll')).toBe(true);
-  //   wrapper.vm.removeNoScroll();
-
-  //   expect(documentElement.classList.contains('cdr-modal__noscroll')).toBeFalsy();
-  //   expect(body.classList.contains('cdr-modal__noscroll')).toBeFalsy();
-
-  // });
 });
