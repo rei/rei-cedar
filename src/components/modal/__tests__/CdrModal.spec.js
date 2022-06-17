1bb774ce8686225f3c6e79b282a5a3ad64671805
Kenji Crosland <kcrosla@rei.com>
2022-06-17 10:31:37 -0500

    test(cdr-modal tests): add commented out tests for modal
    CDR-2541

--- src/components/modal/__tests__/CdrModal.spec.js
+++ src/components/modal/__tests__/CdrModal.spec.js
@@ -26,6 +26,10 @@ describe('CdrModal.vue', () => {
       expect(wrapper.element).toMatchSnapshot();
     });
 
+    it('contains the "cdr-modal__noscroll" class', () => {
+      expect(document.documentElement.classList.contains('cdr-modal__noscroll')).toBe(true);
+    });
+
     it('handleKeyDown', async () => {
       wrapper.trigger('keydown', {
         key: 'a'
@@ -43,6 +47,21 @@ describe('CdrModal.vue', () => {
       await wrapper.vm.$nextTick();
   
       expect(wrapper.emitted().closed.length).toBe(2);
+    });
+
+    describe('after removeNoScroll has been called', ()=>{
+      beforeEach(()=>{
+        wrapper.vm.removeNoScroll();
+      });
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('does NOT contain the "cdr-modal__noscroll" class', () => {
+        expect(document.documentElement.classList.contains('cdr-modal__noscroll')).toBeFalsy();
+        expect(document.body.classList.contains('cdr-modal__noscroll')).toBeFalsy();
+      });
     });
   });
 
@@ -62,6 +81,36 @@ describe('CdrModal.vue', () => {
         },
         slots: {
           default: 'Sticky content',
+        },
+        attachTo: elem,
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+  });
+
+  describe('fullscreen snapshot', ()=>{
+    let wrapper;
+    let elem;
+    beforeEach(()=>{
+      elem = document.createElement('div')
+      if (document.body) {
+        document.body.appendChild(elem)
+      }
+      wrapper = mount(CdrModal, {
+        propsData: {
+          opened: true,
+          label: "Label is the modal title"
+        },
+        data() {
+          return {
+            fullscreen: true,
+          };
+        },
+        slots: {
+          scrollingContentSlot: 'Main content',
         },
         attachTo: elem,
       });
