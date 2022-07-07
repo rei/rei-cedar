2331f5b07e37f8543dbcecce12811324a31a2187
Kenji Crosland <kcrosla@rei.com>
2022-07-07 15:24:22 -0500

    feat(cdr-switch): add beta version of tokens to package.json and add tests
    CDR-2212

--- src/components/switch/__tests__/Switch.spec.js
+++ src/components/switch/__tests__/Switch.spec.js
@@ -1,9 +1,45 @@
-import { shallowMount } from '../../../../test/vue-jest-style-workaround.js';
+import { mount } from '../../../../test/vue-jest-style-workaround.js';
 import CdrSwitch from '../CdrSwitch.vue';
 
 describe('CdrSwitch', () => {
+  let wrapper;
+  beforeEach(()=>{
+    wrapper = mount(CdrSwitch, {
+      props: {
+        id: 'id-123',
+      },
+      slots: {
+        default: ' <span class="custom-text-style">Pull the lever Kronk!</span>'
+      }
+    });
+  })
   it('matches snapshot', () => {
-    const wrapper = shallowMount(CdrSwitch);
     expect(wrapper.element).toMatchSnapshot();
   });
+
+  it('has a role of "switch"', () => {
+    expect(wrapper.find('button').attributes('role')).toBe('switch');
+  });
+
+  it('has an "aria-checked" value of "false"', () => {
+    expect(wrapper.find('button').attributes('aria-checked')).toBe('false');
+  });
+
+  it('the aria-labelledby attribute references the correct id', () => {
+    expect(wrapper.find('button').attributes('aria-labelledby')).toBe('id-123');
+  });
+
+  describe('when the "checked" property is set to true', ()=>{
+    beforeEach(()=>{
+      wrapper.setProps({ checked: true})
+    });
+
+    it('matches snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+    
+    it('has an "aria-checked" value of "false"', () => {
+      expect(wrapper.find('button').attributes('aria-checked')).toBe('true');
+    });
+  })
 });
