6dc32209cda050ec7f6ac74f434f271bf7850a28
Ben Griffin <grif88@gmail.com>
2022-07-18 13:04:13 -0700

    build(vite): add ES6 conventions (drop require, resolve, etc)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
-
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
 import options from './rollupOptions.mjs';
-const packageJson = require('./package.json');
+
+const version = process.env.npm_package_version;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/rei-cedar-next/',
   build: {
     lib: {
-      entry: resolve(__dirname, '/src/lib.js'),
+      entry: './src/lib.js',
       name: 'cedar',
     },
     rollupOptions: options,
@@ -22,18 +22,17 @@ export default defineConfig({
   css: {
     modules: {
       generateScopedName: (name) => {
-        return `${name}_${packageJson.version.replace(/\./g, '-')}`;
+        return `${name}_${version.replace(/\./g, '-')}`;
       }   
     },
   },
   resolve: {
     alias: {
-      'srcdir': resolve(__dirname, 'src'),
-      'cssdir': resolve(__dirname, 'src/css'),
-      'assetsdir': resolve(__dirname, 'src/assets'),
-      'componentsdir': resolve(__dirname, 'src/components'),
-      'mixinsdir': resolve(__dirname, 'src/mixins'),
-      '~': resolve(__dirname, 'node_modules')
+      srcdir: fileURLToPath(new URL('./src', import.meta.url)),
+      cssdir: fileURLToPath(new URL('./src/css', import.meta.url)),
+      componentsdir: fileURLToPath(new URL('./src/components', import.meta.url)),
+      mixinsdir: fileURLToPath(new URL('./src/mixins', import.meta.url)),
+      '~': fileURLToPath(new URL('./node_modules', import.meta.url))
     }
   },
   plugins: [
