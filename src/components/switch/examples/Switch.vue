df0e13301fe4aecd3ac3f45ee01d957874c22784
Kenji Crosland <kcrosla@rei.com>
2022-07-27 14:24:08 -0500

    feat(cdr-switch): add switch component examples
    CDR-2212

--- src/components/switch/examples/Switch.vue
+++ src/components/switch/examples/Switch.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <h2>Toggle Switch</h2>
-    <cdr-grid class="switch-grid">
-      <cdr-switch id="foo-123" v-model="kronkLever">
+    <cdr-grid :class="greenTheme ? 'green-switch-grid' : 'switch-grid'"  :style="{ width: extraWide ? '1000px' : '550px' }" >
+      <cdr-switch id="kronk" v-model="kronkLever">
         Pull the Lever Kronk! (Medium)
       </cdr-switch>
-      <cdr-switch id="foo-345" v-model="kronkLever" :full-width="true">
-        <cdr-text>Pull the Lever Kronk! (Medium: Full Width)</cdr-text>
+      <cdr-switch id="greentheme" v-model="greenTheme" :full-width="true">
+        <cdr-text>Switch to a green theme (Medium: Full Width)</cdr-text>
       </cdr-switch>
-      <cdr-switch id="foo-678" v-model="kronkLever" size="large">
-        <cdr-text>Pull the Lever Kronk! (Large)</cdr-text>
+      <cdr-switch id="extrawide" v-model="extraWide" size="large">
+        <cdr-text>Extra wide switch containers (Large)</cdr-text>
       </cdr-switch>
-      <cdr-switch id="foo-789" v-model="kronkLever" size="large" :full-width="true">
-        Pull the Lever Kronk! (Large: Full Width)
+      <cdr-switch id="foo-789" v-model="happiness" size="large" :full-width="true">
+        Happiness <span v-show="happiness">🙂🙂🙂🙂🙂🙂🙂</span> (Large: Full Width)
       </cdr-switch>
-      <cdr-switch id="foo-101112" v-model="kronkLever">
-        <cdr-text class="custom-text-style">Pull the lever with custom text styling</cdr-text>
+      <cdr-switch id="customtext" v-model="customTextStyle">
+        <cdr-text :class="customTextStyle ? 'custom-text-style' : ''">Add Custom Switch Label Styling</cdr-text>
       </cdr-switch>
-      <img class="kronk" :style="{ maxHeight: kronkLever ? '500px' : 0 }"
+      <img class="switchable-image" :style="{ maxHeight: kronkLever ? '500px' : 0 }"
         src="https://c.tenor.com/hPZScif7OvwAAAAC/pull-lever.gif" alt="">
     </cdr-grid>
   </div>
@@ -30,7 +30,11 @@ export default {
   name: 'Switch',
   data() {
     return {
-      kronkLever: false
+      kronkLever: false,
+      greenTheme: false,
+      extraWide: false,
+      happiness: false,
+      customTextStyle: false,
     }
   },
   components: {
@@ -40,10 +44,9 @@ export default {
 </script>
 
 <style>
-.switch-grid {
+.switch-grid, .green-switch-grid {
   padding: 2rem;
   gap: 2rem;
-  width: 550px;
 }
 
 .custom-text-style {
@@ -51,12 +54,17 @@ export default {
   font-weight: bold;
 }
 
-.switch-grid>div[class^="cdr-switch"] {
+.switch-grid > div[class^="cdr-switch"] {
   border: 1px solid #dcd6cb;
   padding: 1rem;
 }
 
-.kronk {
+.green-switch-grid > div[class^="cdr-switch"] {
+  border: 5px solid green;
+  padding: 1rem;
+}
+
+.switchable-image {
   transition: max-height 1.15s ease-out;
   margin: auto;
   width: 500px;
