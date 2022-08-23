f464078c5716e3204a068d7c7c01ed47cf299511
Ben Griffin <grif88@gmail.com>
2022-08-23 11:06:52 -0700

    style(text): fix code style

--- src/components/text/CdrText.vue
+++ src/components/text/CdrText.vue
@@ -1,14 +1,6 @@
-<template>
-  <component
-    :is="tag"
-    :class="style['cdr-text']"
-  >
-    <slot />
-  </component>
-</template>
-
 <script>
 import { useCssModule, defineComponent } from 'vue';
+
 export default defineComponent({
   name: 'CdrText',
   props: {
@@ -20,10 +12,19 @@ export default defineComponent({
   setup() {
     return {
       style: useCssModule(),
-    }
-  }
+    };
+  },
 });
 </script>
+
+<template>
+  <component
+    :is="tag"
+    :class="style['cdr-text']"
+  >
+    <slot />
+  </component>
+</template>
 
 <style lang="scss" module src="./styles/CdrText.module.scss">
 </style>
