73603e888676a4d16f16967071645d5b34ea357f
Kenji Crosland <kcrosla@rei.com>
2022-08-24 11:27:55 -0500

    refactor(cdr-tooltip): remove script setup

--- src/components/tooltip/CdrTooltip.vue
+++ src/components/tooltip/CdrTooltip.vue
@@ -1,80 +1,147 @@
-<script setup>
-import { useCssModule, useSlots, ref, onMounted, watch } from 'vue';
+<script>
+import { useCssModule, defineComponent, defineEmits, useSlots, ref, onMounted, watch } from 'vue';
 import mapClasses from '../../utils/mapClasses.js';
 import CdrPopup from '../popup/CdrPopup.vue';
 import propValidator from '../../utils/propValidator.js';
-
-const props = defineProps({
-  position: {
-    type: String,
-    required: false,
-    default: 'top',
-    validator: (value) => propValidator(
-      value,
-      ['top', 'bottom', 'left', 'right'],
-    ),
+export default defineComponent({
+  name: 'CdrTooltip',
+        components: {
+    CdrPopup,
   },
-  autoPosition: {
-    type: Boolean,
-    default: true,
+  props: {
+    position: {
+      type: String,
+      required: false,
+      default: 'top',
+      validator: (value) => propValidator(
+        value,
+        ['top', 'bottom', 'left', 'right'],
+      ),
+    },
+    autoPosition: {
+      type: Boolean,
+      default: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    contentClass: {
+      type: String,
+      required: false,
+    },
+    open: {
+      type: Boolean,
+      default: false,
+    },
   },
-  id: {
-    type: String,
-    required: true,
-  },
-  contentClass: {
-    type: String,
-    required: false,
-  },
-  open: {
-    type: Boolean,
-    default: false,
-  },
-});
-const slots = useSlots();
-const emit = defineEmits(['opened', 'closed']);
-const style = useCssModule();
+  setup(props, ctx) {
+    const slots = useSlots();
+    //const emit = defineEmits(['opened', 'closed']);
+    const style = useCssModule();
 
-const isOpen = ref(false);
-let timeout;
-let popupElement;
-const popupEl = ref(null);
-const triggerEl = ref(null);
-const hasTrigger = slots['trigger'];
+    const isOpen = ref(false);
+    let timeout;
+    let popupElement;
+    const popupEl = ref(null);
+    const triggerEl = ref(null);
+    const hasTrigger = slots['trigger'];
 
-const openTooltip = (e) => {
-  if (timeout) clearTimeout(timeout);
-  isOpen.value = true;
-  emit('opened', e);
-};
-const closeTooltip = (e) => {
-  timeout = setTimeout(() => {
-    isOpen.value = false;
-    emit('closed', e);
-  }, 250);
-};
-const addHandlers = () => {
-  const triggerElement = triggerEl.value.children[0];
-  popupElement = popupEl.value?.$el;
-  if (triggerElement) {
-    triggerElement.addEventListener('mouseover', openTooltip);
-    triggerElement.addEventListener('focus', openTooltip);
+    const openTooltip = (e) => {
+      if (timeout) clearTimeout(timeout);
+      isOpen.value = true;
+      ctx.emit('opened', e);
+    };
+    const closeTooltip = (e) => {
+      timeout = setTimeout(() => {
+        isOpen.value = false;
+        ctx.emit('closed', e);
+      }, 250);
+    };
+    const addHandlers = () => {
+      const triggerElement = triggerEl.value.children[0];
+      popupElement = popupEl.value?.$el;
+      if (triggerElement) {
+        triggerElement.addEventListener('mouseover', openTooltip);
+        triggerElement.addEventListener('focus', openTooltip);
 
-    triggerElement.addEventListener('mouseleave', closeTooltip);
-    triggerElement.addEventListener('blur', closeTooltip);
+        triggerElement.addEventListener('mouseleave', closeTooltip);
+        triggerElement.addEventListener('blur', closeTooltip);
 
-    popupElement.addEventListener('mouseover', openTooltip);
-    popupElement.addEventListener('mouseleave', closeTooltip);
+        popupElement.addEventListener('mouseover', openTooltip);
+        popupElement.addEventListener('mouseleave', closeTooltip);
+      }
+    };
+
+    watch(() => props.open, () => (props.open ? openTooltip() : closeTooltip()));
+
+    onMounted(() => {
+      addHandlers();
+      const trigger = triggerEl.value.children[0];
+      if (trigger) trigger.setAttribute('aria-describedby', props.id);
+    });
+
+    return {
+      slots,
+      style,
+      isOpen,
+      popupElement,
+      popupEl,
+      triggerEl,
+      hasTrigger,
+      openTooltip,
+      closeTooltip,
+      addHandlers,
+      mapClasses
+    }
   }
-};
+})
 
-watch(() => props.open, () => (props.open ? openTooltip() : closeTooltip()));
 
-onMounted(() => {
-  addHandlers();
-  const trigger = triggerEl.value.children[0];
-  if (trigger) trigger.setAttribute('aria-describedby', props.id);
-});
+// const slots = useSlots();
+// const emit = defineEmits(['opened', 'closed']);
+// const style = useCssModule();
+
+// const isOpen = ref(false);
+// let timeout;
+// let popupElement;
+// const popupEl = ref(null);
+// const triggerEl = ref(null);
+// const hasTrigger = slots['trigger'];
+
+// const openTooltip = (e) => {
+//   if (timeout) clearTimeout(timeout);
+//   isOpen.value = true;
+//   emit('opened', e);
+// };
+// const closeTooltip = (e) => {
+//   timeout = setTimeout(() => {
+//     isOpen.value = false;
+//     emit('closed', e);
+//   }, 250);
+// };
+// const addHandlers = () => {
+//   const triggerElement = triggerEl.value.children[0];
+//   popupElement = popupEl.value?.$el;
+//   if (triggerElement) {
+//     triggerElement.addEventListener('mouseover', openTooltip);
+//     triggerElement.addEventListener('focus', openTooltip);
+
+//     triggerElement.addEventListener('mouseleave', closeTooltip);
+//     triggerElement.addEventListener('blur', closeTooltip);
+
+//     popupElement.addEventListener('mouseover', openTooltip);
+//     popupElement.addEventListener('mouseleave', closeTooltip);
+//   }
+// };
+
+// watch(() => props.open, () => (props.open ? openTooltip() : closeTooltip()));
+
+// onMounted(() => {
+//   addHandlers();
+//   const trigger = triggerEl.value.children[0];
+//   if (trigger) trigger.setAttribute('aria-describedby', props.id);
+// });
 </script>
 
 <template>
