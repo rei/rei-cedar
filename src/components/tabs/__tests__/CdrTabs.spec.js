d51565a1a44c3e0741c7897c50c0c2a0ff1b1f7d
Kenji Crosland <kcrosla@rei.com>
2022-09-01 13:27:01 -0500

    test(cdr-tabs): refactor cdr-tabs tests
    CDR-2541

--- src/components/tabs/__tests__/CdrTabs.spec.js
+++ src/components/tabs/__tests__/CdrTabs.spec.js
@@ -2,20 +2,20 @@ import { mount } from '../../../../test/vue-jest-style-workaround.js';
 import CdrTabs from '../CdrTabs.vue';
 import CdrTabPanel from '../CdrTabPanel.vue';
 import { h } from 'vue';
-// Tests use nextTick because of the nextTick in mounted hook of tabs
 
 describe('CdrTabs', () => {
   let wrapper;
-  beforeEach(()=>{
+  beforeEach(() => {
     wrapper = mount(CdrTabs, {
       slots: {
         default: [
-          h(CdrTabPanel, {name: 'tab1', 'aria-labelledby': 'tab-small-one',}), 
-          h(CdrTabPanel, {name: 'tab2'}),
-          h(CdrTabPanel, {name: 'tab3', disabled: true}),
-          h(CdrTabPanel, {name: 'tab4'})
+          h(CdrTabPanel, { name: 'tab1', 'aria-labelledby': 'tab-small-one', }),
+          h(CdrTabPanel, { name: 'tab2' }),
+          h(CdrTabPanel, { name: 'tab3', disabled: true }),
+          h(CdrTabPanel, { name: 'tab4' })
         ],
       },
+      attachTo: document.body
     });
   })
   describe('mounted', () => {
@@ -23,92 +23,95 @@ describe('CdrTabs', () => {
       expect(wrapper.element).toMatchSnapshot();
     });
 
-    it('mounts with cdr-tab-panel children', async () => {
-      const spyCalculateOverflow = vi.spyOn(wrapper.vm, 'calculateOverflow');
-      const spyUpdateUnderline = vi.spyOn(wrapper.vm, 'updateUnderline');
-
-    //   await wrapper.vm.$nextTick();
-
+    it('has the expected number of tabs', () => {
       expect(wrapper.vm.tabs.length).toBe(4);
       expect(wrapper.findAll('button').length).toBe(4);
-      expect(wrapper.vm.selectedIndex).toBe(0);
-      setTimeout(() => { // for debounce
-        expect(spyCalculateOverflow).toHaveBeenCalled();
-        expect(spyUpdateUnderline).toHaveBeenCalled();
-      }, 500);
     });
+
+    it('selects the first index by default', () => {
+      expect(wrapper.vm.selectedIndex).toBe(0);
+    });
+
+    //TODO: Acceptance or Visual test. These methods never got called in the test. setTimeout is obfuscating that it doesn't work
+
+    // it('mounts with cdr-tab-panel children',  (done) => {
+    //   setTimeout(() => { // for debounce
+    //     expect(spyCalculateOverflow).toHaveBeenCalled();
+    //     expect(spyUpdateUnderline).toHaveBeenCalled();
+    //     done();
+    //   }, 500);
+    // });
   });
 
   describe('event listeners', () => {
     it('handles scroll event', async () => {
-      const spyCalculateOverflow = vi.spyOn(wrapper.vm, 'calculateOverflow');
-      const spyUpdateUnderline = vi.spyOn(wrapper.vm, 'updateUnderline');
-
       wrapper.find('.cdr-tabs__header-container').element.dispatchEvent(new Event('scroll'));
-      await wrapper.vm.$nextTick();
+      expect(wrapper.vm.overflowLeft).toBe(false);
 
-      setTimeout(() => { // for debounce
-        expect(wrapper.vm.overflowLeft).toBe(false);
-        expect(spyCalculateOverflow).toHaveBeenCalled();
-        expect(spyUpdateUnderline).toHaveBeenCalled();
-      }, 600);
+      //TODO: Acceptance or Visual test. These methods never got called in the test. setTimeout is obfuscating that it doesn't work
+
+      // setTimeout(() => { // for debounce
+      //   expect(wrapper.vm.overflowLeft).toBe(false);
+      //   expect(spyCalculateOverflow).toHaveBeenCalled();
+      //   expect(spyUpdateUnderline).toHaveBeenCalled();
+      // }, 600);
     });
 
-    it('handles resize event', async () => {
-      const spyCalculateOverflow = vi.spyOn(wrapper.vm, 'calculateOverflow');
-      const spyUpdateUnderline = vi.spyOn(wrapper.vm, 'updateUnderline');
-      const spyGetHeaderWidth = vi.spyOn(wrapper.vm, 'getHeaderWidth');
-      window.dispatchEvent(new Event('resize'));
-      await wrapper.vm.$nextTick();
+    //TODO: Acceptance or Visual test. These methods never got called in the test. setTimeout is obfuscating that it doesn't work
 
-      setTimeout(() => { // for debounce
-        expect(spyGetHeaderWidth).toHaveBeenCalledTimes(2);
-        expect(spyCalculateOverflow).toHaveBeenCalledTimes(2);
-        expect(spyUpdateUnderline).toHaveBeenCalledTimes(2);
-      }, 500);
-    });
+    // it('handles resize event', async () => {
+    //   window.dispatchEvent(new Event('resize'));
+
+    // setTimeout(() => { // for debounce
+    //   expect(spyGetHeaderWidth).toHaveBeenCalledTimes(2);
+    //   expect(spyCalculateOverflow).toHaveBeenCalledTimes(2);
+    //   expect(spyUpdateUnderline).toHaveBeenCalledTimes(2);
+    // }, 500);
+    // });
   });
 
-  it('getNextTab and getPreviousTab', async() => {
-    expect(wrapper.vm.selectedIndex).toBe(0);
+  it('selectTabNext() selects changes the selectedIndex as expected', () => {
+    wrapper.vm.selectedIndex = 0;
     wrapper.vm.selectTabNext();
     expect(wrapper.vm.selectedIndex).toBe(1);
+  })
+
+  it('selectTabPrev() selects changes the selectedIndex as expected', () => {
+    wrapper.vm.selectedIndex = 1;
+    wrapper.vm.selectTabPrev();
+    expect(wrapper.vm.selectedIndex).toBe(0);
+  })
+
+  it('skips disabled tabs with selectTabNext()', () => {
+    wrapper.vm.selectedIndex = 2;
     wrapper.vm.selectTabNext();
-    //TODO: There should be a separate it block to test that disabled tabs are skipped
     expect(wrapper.vm.selectedIndex).toBe(3);
+  })
+
+  it('skips disabled tabs with selectTabPrev()', () => {
+    wrapper.vm.selectedIndex = 3;
     wrapper.vm.selectTabPrev();
     expect(wrapper.vm.selectedIndex).toBe(1);
-    wrapper.vm.selectTabPrev();
-    expect(wrapper.vm.selectedIndex).toBe(0);
-  });
+  })
 
-  describe('handleClick', () => {
-    it('changes the selectedIndex', async () => {
-      await wrapper.vm.$nextTick();
-      expect(wrapper.vm.selectedIndex).toBe(0);
+  describe('selectTab()', () => {
+    it('changes the selectedIndex', () => {
+      wrapper.vm.selectedIndex = 0;
       wrapper.findAll('button')[1].trigger('click');
-      await wrapper.vm.$nextTick();
       expect(wrapper.vm.selectedIndex).toBe(1);
     });
   });
 
-  it('handles right arrow key', async () => {
-    await wrapper.vm.$nextTick();
-    expect(wrapper.vm.selectedIndex).toBe(0);
-    // Trigger right arrow keyup event
+  it('handles right arrow key', () => {
+    wrapper.vm.selectedIndex = 0;
     wrapper.findAll('button')[0].trigger('keyup.right');
-    await wrapper.vm.$nextTick();
     expect(wrapper.vm.selectedIndex).toBe(1);
   });
 
   it('handles left arrow key', async () => {
-    wrapper.vm.selectedIndex = 1;
-    await wrapper.vm.$nextTick();
-    expect(wrapper.vm.selectedIndex).toBe(1);
-    // Trigger left arrow keyup event
+    wrapper.vm.selectedIndex = 2;
     wrapper.findAll('button')[1].trigger('keyup.left');
-    await wrapper.vm.$nextTick();
-    expect(wrapper.vm.selectedIndex).toBe(0);
+    expect(wrapper.vm.selectedIndex).toBe(1);
   });
 
   describe('overflow classes', () => {
@@ -128,25 +131,36 @@ describe('CdrTabs', () => {
     });
   });
 
-  it('accessibility', async () => {
-    const tab1 = wrapper.find('#tab-small-one');
-    //TODO: Separate describe block for accessibility with single it blocks for each of these expect statements
-    expect(tab1.attributes()['aria-selected']).toBe('true');
-    expect(tab1.attributes()['role']).toBe('tab');
-    expect(wrapper.find('.cdr-tabs__header').attributes()['role']).toBe('presentation');
-    expect(wrapper.find('.cdr-tabs__header-container').attributes()['role']).toBe('tablist');
-  });
+  describe('accessibility', () => {
+    let tab1;
+    beforeEach(() => {
+      tab1 = wrapper.find('#tab-small-one');
+    })
+
+    it('selected tab has the expect aria-selected value', async () => {
+      expect(tab1.attributes()['aria-selected']).toBe('true');
+    });
+
+    it('has the correct tab role', async () => {
+      expect(tab1.attributes()['role']).toBe('tab');
+    });
+
+    it('the tab header has the correct presentation role', async () => {
+      expect(wrapper.find('.cdr-tabs__header').attributes()['role']).toBe('presentation');
+    });
+
+    it('the tab container has the correct tablist role', async () => {
+      expect(wrapper.find('.cdr-tabs__header-container').attributes()['role']).toBe('tablist');
+    });
+  })
 
   it('focuses on the selected tab button element', async () => {
     await wrapper.vm.selectTab(1);
-    setTimeout(()=>{
-      expect(wrapper.findAll('button')[1]).toBe(document.activeElement);
-    }, 500);
+    expect(wrapper.findAll('button')[1].element).toBe(document.activeElement);
   });
 
   it('calculateOverflow', async () => {
-    // wrapper.setData({ headerWidth: 2000 });
-    await wrapper.vm.$nextTick();
+    //wrapper.setProps({ headerWidth: 2000 });
     wrapper.vm.calculateOverflow();
     await wrapper.vm.$nextTick();
     expect(wrapper.vm.headerOverflow).toBe(true);
