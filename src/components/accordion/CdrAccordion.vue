b458b8bfea745d03182f3702802c1e97ca4eab62
Kurt Medley <kmedley@rei.com>
2022-08-31 18:00:20 -0700

    In the context of SSR, watch callback doesnt get executed without using {immediate:true} option. Setting a default value for maxHeight seems to satisfy everything

--- src/components/accordion/CdrAccordion.vue
+++ src/components/accordion/CdrAccordion.vue
@@ -121,6 +121,7 @@ export default defineComponent({
         focus: onFocus,
         blur: onBlur,
       }));
+    maxHeight.value = 0;
     watch(() => props.opened, (opened) => {
       maxHeight.value = !opened ? `${accordionContentEl.value.clientHeight}px` : 0;
       // nextTick is not sufficient here, must wait for CSS to re-paint
