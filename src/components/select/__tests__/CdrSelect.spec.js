50b87431b44daff56fec21003794bf59910d798f
Kenji Crosland <kcrosla@rei.com>
2022-09-01 10:49:35 -0500

    test(cdr-select): refactor cdr-select tests
    CDR-2541

--- src/components/select/__tests__/CdrSelect.spec.js
+++ src/components/select/__tests__/CdrSelect.spec.js
@@ -3,126 +3,359 @@ import { mount } from '@vue/test-utils';
 import CdrSelect from '../CdrSelect.vue';
 
 describe('CdrSelect', () => {
-  it('renders correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'Label Test',
-        id: 'renders'
-      },
+  describe('default configuration with a name attribute', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'Label Test',
+          id: 'renders'
+        },
+        attrs: {
+          name: 'testing',
+        }
+      });
+    })
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.element).toMatchSnapshot();
-  });
 
-  it('renders error state correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'Label Test',
-        id: 'renders',
-        error: 'What happened?'
-      },
+    it('sets select name attribute correctly', () => {
+      expect(wrapper.find('select').attributes('name')).toBe('testing');
     });
-    expect(wrapper.element).toMatchSnapshot();
-  });
 
-  it('hide-label applies sr-only to label element', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        hideLabel: true,
-        id: 'test',
-      },
+    describe('with an error state', () => {
+      beforeEach(() => {
+        wrapper.setProps({ error: 'What happened?' })
+      })
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('renders error text', () => {
+        expect(wrapper.find('.cdr-form-error').text()).toBe('What happened?');
+      });
     });
-    expect(wrapper.find('label').classes()).toContain('cdr-label-standalone__label--sr-only');
-  });
 
-  it('renders a prompt', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        prompt: 'what is it',
-        id: 'test',
-      },
+    describe('with a hide-label prop', () => {
+      beforeEach(() => {
+        wrapper.setProps({ hideLabel: true })
+      })
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('it applies sr-only to label element', () => {
+        expect(wrapper.find('label').classes()).toContain('cdr-label-standalone__label--sr-only');
+      });
     });
-    expect(wrapper.find('option').text()).toBe('what is it');
-  });
 
-  it('sets select name attribute correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'Label Test',
-        id: 'test',
-      },
-      attrs: {
-        name: 'testing',
-      }
+    describe('with a prompt prop', () => {
+      beforeEach(() => {
+        wrapper.setProps({ prompt: 'what is it' })
+      })
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('renders the prompt', () => {
+        expect(wrapper.find('option').text()).toBe('what is it');
+      });
     });
-    expect(wrapper.find('select').attributes('name')).toBe('testing');
-  });
 
-  it('sets select disabled attribute correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-      },
-      attrs: {
-        disabled: true,
-      }
+
+    describe('with a required prop', () => {
+      beforeEach(() => {
+        wrapper.setProps({ required: true })
+      })
+
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('sets select required attribute correctly', () => {
+        expect(wrapper.find('select').attributes('aria-required')).toBe('true');
+      });
     });
-    expect(wrapper.find('select').attributes('disabled')).toBe('');
-  });
+    describe('with a required prop', () => {
+      beforeEach(() => {
+        wrapper.setProps({
+          multiple: true,
+          value: []
+        })
+      })
 
-  it('sets select disabled caret classname correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-      },
-      attrs: {
-        disabled: true,
-      }
+      it('renders correctly', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('sets select multiple attribute correctly', () => {
+        expect(wrapper.find('select').attributes('multiple')).toBe('');
+      });
     });
-    const caret = wrapper.find('svg');
-    expect(caret.classes()).toContain('cdr-select__caret--disabled');
-  });
+  })
 
-  it('sets select required attribute correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        required: true,
-        id: 'test',
-      },
+  describe('with "disabled" set to true', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'Label Test',
+          id: 'renders'
+        },
+        attrs: {
+          disabled: true,
+        }
+      });
+    })
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
     });
-    expect(wrapper.find('select').attributes('aria-required')).toBe('true');
-  });
 
-  it('sets select autofocus attribute correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-      },
-      attrs: {
-        autofocus: true,
-      }
+    it('sets select disabled attribute correctly', () => {
+      expect(wrapper.find('select').attributes('disabled')).toBe('');
     });
-    expect(wrapper.find('select').attributes('autofocus')).toBe('');
-  });
 
-  it('sets select multiple attribute correctly', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'Label Test',
-        multiple: true,
-        value: [],
-        id: 'test',
-      },
+    it('sets select disabled caret classname correctly', () => {
+      const caret = wrapper.find('svg');
+      expect(caret.classes()).toContain('cdr-select__caret--disabled');
     });
-    expect(wrapper.find('select').attributes('multiple')).toBe('');
-  });
+  })
 
-// TODO: something wrong with v-model binding for CdrSelect?
+  describe('with "autofocus" set to true', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'Label Test',
+          id: 'renders'
+        },
+        attrs: {
+          autofocus: true,
+        }
+      });
+    })
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('sets select autofocus attribute correctly', () => {
+      expect(wrapper.find('select').attributes('autofocus')).toBe('');
+    });
+  })
+
+  describe('using the error slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'test',
+        },
+        slots: {
+          'error': 'whoops',
+        },
+      });
+    })
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('does not render error slot when error state is inactive', async () => {
+      wrapper.setProps({ error: false });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find('.cdr-form-error').exists()).toBe(false);
+    });
+
+    it('renders the slot when error state is active', async () => {
+      wrapper.setProps({ error: true });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find('.cdr-form-error').text()).toBe('whoops');
+    });
+  })
+
+  describe('component with options and values set', () => {
+    let wrapper;
+    let select;
+    let options;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          value: '4',
+          id: 'test',
+          modelValue: '4',
+          options: ['3', '4'],
+        },
+      });
+      select = wrapper.find('select');
+      options = select.findAll('option');
+    })
+
+    it('has the expected initial state', async () => {
+      expect(select.element.value).toBe('4');
+      expect(options[1].element.selected).toBeTruthy();
+    });
+
+    it('has the expected state after a modelValue change', async () => {
+      wrapper.setProps({ modelValue: '3' });
+      await wrapper.vm.$nextTick();
+      expect(select.element.value).toBe('3');
+      expect(options[0].element.selected).toBeTruthy();
+    });
+  })
+
+  describe('with info action slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'info-action'
+        },
+        slots: {
+          'info-action': '🤠',
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders info action slot', () => {
+      expect(wrapper.find('.cdr-label-standalone__info-action').text()).toBe('🤠');
+    });
+  })
+
+  describe('with helper text slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'aria-test'
+        },
+        slots: {
+          'helper-text': 'yee haw',
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders info action slot', () => {
+      expect(wrapper.find('.cdr-label-standalone__helper').text()).toBe('yee haw');
+    });
+
+    it('helper text slots are linked to select via aria-describedby', () => {
+      expect(wrapper.find('select').attributes('aria-describedby')).toBe('aria-test-helper-text-top');
+    });
+  })
+
+  describe('with info slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'info-action',
+        },
+        slots: {
+          'info': 'howdy',
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders info action slot', () => {
+      expect(wrapper.find('.cdr-label-standalone__info').text()).toBe('howdy');
+    });
+  })
+
+  describe('with pre-icon slot', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'test',
+        },
+        slots: {
+          'pre-icon': '🤠',
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('renders info action slot', () => {
+      expect(wrapper.find('.cdr-select__pre-icon').text()).toBe('🤠');
+    });
+  })
+
+  describe('with aria-describedby and id provided', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'aria-test',
+        },
+        attrs: {
+          'aria-describedby': 'foo',
+        },
+        slots: {
+          'helper-text': 'extremely helpful'
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('dynamic aria-describedby is merged with native attr', () => {
+      expect(wrapper.find('select').attributes('aria-describedby')).toBe('aria-test-helper-text-top foo');
+    });
+  })
+
+  describe('with no attr or helper slots', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(CdrSelect, {
+        propsData: {
+          label: 'test',
+          id: 'aria-test',
+        },
+      });
+    });
+
+    it('renders correctly', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    it('does not apply aria-describedby', () => {
+      expect(wrapper.find('select').attributes('aria-describedby')).toBe(undefined);
+    });
+  })
+
+
+
+  // TODO: something wrong with v-model binding for CdrSelect?
   // xit('emits change event with correct value', async () => {
   //   const wrapper = mount(CdrSelect, {
   //     propsData: {
@@ -172,151 +405,4 @@ describe('CdrSelect', () => {
   //   expect(wrapper.emitted().change[0][0]).toEqual(['1', '3']);
   //   expect(wrapper.emitted().change[0][1] instanceof Event).toBeTruthy();
   // });
-
-  it('updating v-model data updates the select', async () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        value: '4',
-        id: 'test',
-        options: ['3', '4'],
-      },
-    });
-    const select = wrapper.find('select');
-    const options = select.findAll('option');
-    wrapper.setProps({modelValue: '3'});
-    await wrapper.vm.$nextTick();
-    expect(select.element.value).toBe('3');
-    expect(options[0].element.selected).toBeTruthy();
-  });
-
-  it('renders info action slot', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'info-action'
-      },
-      slots: {
-        'info-action': '🤠',
-      },
-    });
-    expect(wrapper.find('.cdr-label-standalone__info-action').text()).toBe('🤠');
-  });
-
-  it('renders helper-text slot', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'info-action'
-      },
-      slots: {
-        'helper-text': 'yee haw',
-      },
-    });
-    expect(wrapper.find('.cdr-label-standalone__helper').text()).toBe('yee haw');
-  });
-
-  it('renders info slot', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'info-action',
-      },
-      slots: {
-        'info': 'howdy',
-      },
-    });
-    expect(wrapper.find('.cdr-label-standalone__info').text()).toBe('howdy');
-  });
-
-  it('renders pre-icon slot', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-      },
-      slots: {
-        'pre-icon': '🤠',
-      },
-    });
-    expect(wrapper.find('.cdr-select__pre-icon').text()).toBe('🤠');
-  });
-
-  it('renders error slot when error state is active', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-        error: true,
-      },
-      slots: {
-        'error': 'whoops',
-      },
-    });
-    expect(wrapper.find('.cdr-form-error').text()).toBe('whoops');
-  });
-
-  it('renders text when passed as error prop', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-        error: 'wrong!',
-      },
-    });
-    expect(wrapper.find('.cdr-form-error').text()).toBe('wrong!');
-  });
-
-  it('does not render error slot when error state is inactive', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'test',
-        error: false,
-      },
-      slots: {
-        'error': 'whoops',
-      },
-    });
-    expect(wrapper.find('.cdr-form-error').exists()).toBe(false);
-  });
-
-  it('helper text slots are linked to select via aria-describedby', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'aria-test',
-      },
-      slots: {
-        'helper-text': 'extremely helpful'
-      },
-    });
-    expect(wrapper.find('select').attributes('aria-describedby')).toBe('aria-test-helper-text-top');
-  });
-
-  it('dynamic aria-describedby is merged with native attr', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'aria-test',
-      },
-      attrs: {
-        'aria-describedby': 'foo',
-      },
-      slots: {
-        'helper-text': 'extremely helpful'
-      },
-    });
-    expect(wrapper.find('select').attributes('aria-describedby')).toBe('aria-test-helper-text-top foo');
-  });
-
-  it('does not apply aria-describedby if attr or helper slots are not present', () => {
-    const wrapper = mount(CdrSelect, {
-      propsData: {
-        label: 'test',
-        id: 'aria-test',
-      },
-    });
-    expect(wrapper.find('select').attributes('aria-describedby')).toBe(undefined);
-  });
 });
