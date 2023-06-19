ed7453c7c5f07f2a02dba2a1827b5ae4f0ace541
Ben Griffin <grif88@gmail.com>
2023-06-19 11:24:46 -0700

    refactor(utils): make returned type consistently string for buildBooleanClass

--- src/utils/buildClass.ts
+++ src/utils/buildClass.ts
@@ -1,4 +1,3 @@
-// TODO: make arg order consistent
 
 /**
  * Returns a modified base class
@@ -25,7 +24,7 @@ const modifyClassName = (base: string, modifier: string) => `${base}--${modifier
  * This takes that and returns classes of "cdr-table--full-width@sm cdr-table--full-width@lg"
  */
 
-// TODO: I think I want to swap the order of modifier and prop args here, currently passing an empty string for modifier half the time.
+
 const responsiveModifyClass = (base: string, modifier: string, prop: string|boolean) => {
   if (typeof prop === 'string') {
     return prop.split(' ')
@@ -34,10 +33,12 @@ const responsiveModifyClass = (base: string, modifier: string, prop: string|bool
   }
   return modifyClassName(base, modifier);
 };
-// TODO: combine these?
+
 const buildBooleanClass = (baseClass: string, prop: string|boolean, name: string) => {
   if (typeof prop === 'boolean') {
-    return prop && modifyClassName(baseClass, name);
+    return prop
+      ? modifyClassName(baseClass, name)
+      : '';
   }
 
   return responsiveModifyClass(baseClass, name, prop);
@@ -48,6 +49,3 @@ export {
   responsiveModifyClass,
   buildBooleanClass,
 };
-
-// TODO: REFACTOR this stuff to make more sense!!!!!
-// buildclass name should reflect the prop type!!!!
