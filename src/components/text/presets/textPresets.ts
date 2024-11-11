c52ab205937a9be726fd0bb65fe37cf2d87c4562
sikhote <24573542+sikhote@users.noreply.github.com>
2024-11-11 18:59:20 +0000

    Deploying to gh-pages from @ rei/rei-cedar@fad20e955593cb0f1f61fb1dbf2a706def83a67b 🚀

--- src/components/text/presets/textPresets.ts
+++
@@ -1,8 +0,0 @@
-export { default as CdrHeadingDisplay } from './CdrHeadingDisplay.vue';
-export { default as CdrHeadingSerif } from './CdrHeadingSerif.vue';
-export { default as CdrHeadingSans } from './CdrHeadingSans.vue';
-export { default as CdrSubheadingSans } from './CdrSubheadingSans.vue';
-export { default as CdrUtilitySans } from './CdrUtilitySans.vue';
-export { default as CdrUtilitySerif } from './CdrUtilitySerif.vue';
-export { default as CdrBody } from './CdrBody.vue';
-export { default as CdrEyebrow } from './CdrEyebrow.vue';
--- src/props/background.ts
+++
@@ -1,7 +0,0 @@
-import propValidator from '../utils/propValidator';
-
-export default {
-  type: [String],
-  default: 'primary',
-  validator: (value: string) => propValidator(value, ['primary', 'secondary']),
-};
