109966a086c2405f9a02a6af87bcd422a5bc31b2
sikhote <24573542+sikhote@users.noreply.github.com>
2024-11-25 18:38:26 +0000

    Deploying to gh-pages from @ rei/rei-cedar@7a15177761b01117df83164a9f1dbf04e3ef29ad 🚀

--- browserTargets.mjs
+++
@@ -1,7 +0,0 @@
-export default [
-  'Chrome >= 111',
-  'Firefox >= 121',
-  'iOS >= 15.6',
-  'Safari >= 15.6',
-  'Edge >= 120',
-];
--- src/utils/mapClasses.ts
+++
@@ -1,8 +0,0 @@
-export default function mapClasses(style: object, ...classes: string[]) {
-  return classes.reduce((acc: string[], el: string) => acc.concat((el || '').split(' ')),
-    []
-  )
-  .map((className: string) => style[className as keyof object])
-  .filter((x) => x)
-  .join(' ');
-}
