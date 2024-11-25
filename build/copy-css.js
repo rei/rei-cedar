c041b9d47ff9df67ab18be7f356044e297218e83
Michael Hewson <mhewson@gmail.com>
2024-11-25 14:48:01 -0800

    updating build

--- build/copy-css.js
+++ build/copy-css.js
@@ -7,6 +7,6 @@ const fs = require('fs-extra');
 
 const DIST = 'dist';
 
-['cdr-fonts.css', 'cdr-reset.css'].forEach(file => {
+['cdr-fonts.css', 'cdr-reset.css', 'cdr-palette.css'].forEach(file => {
   fs.copyFileSync(path.join(DIST, '/style', file), path.join(DIST, file));
 })
