7a35b8c8770c6fef6a74c5a0386e075230757b04
Michael Hewson <mhewson@gmail.com>
2024-11-25 10:36:00 -0800

    component updates for palette v1

--- src/components/surface/CdrSurface.vue
+++ src/components/surface/CdrSurface.vue
@@ -9,23 +9,53 @@ import { getSurfaceProps } from '../../utils/surface';
 defineOptions({ name: 'CdrSurface' });
 
 const props = withDefaults(defineProps<surface>(), {
+    /**
+   * sets the background color 
+   * @values primary, secondary, brand-spruce, sale,
+   */
   background: 'primary',
+   /**
+   * If the withBorder prop is true this prop specifies a border color based on the token options within Cedar.  
+   * @values primary, secondary, success, warning, error, info
+   */
   borderColor: 'primary',
+  /**
+   * If the withBorder prop is true this prop will set the surface borders style 
+   * @values zero, sixteenth-x, eighth-x, three-sixteenth-x, quarter-x'
+   */
   borderStyle: 'solid',
+   /**
+   * If the withBorder prop is true this prop will set the surface borders width
+   * @values zero, sixteenth-x, eighth-x, three-sixteenth-x, quarter-x'
+   */
   borderWidth: 'sixteenth-x',
+  /**
+   * sets background color and border color to undefined 
+   * @values default
+   */
   modifier: 'default',
+  /**
+   * Sets border radius. 
+   * @values sharp, soft, softer, softest, round
+   */
   radius: 'sharp',
   /**
-   * Sets Adds a shadow based on the token options within Cedar. 
+   * Sets Adds a box-shadow based on the token options within Cedar. 
    * @values flat, raised, elevated, floating, lifted
    */
   shadow: 'flat',
   /**
    * Sets valid HTML element tag
-   * @values a, button
    */
   tag: 'div',
+  /**
+   * Surface will display with border.  default color, width, and style.
+   */
   withBorder: false,
+  /**
+   * Alters the palette context to a new or provided palette.
+   *  @values default, membership-subtle, membership-vibrant, sandstone
+   */
   palette: 'default',
 });
 
@@ -44,7 +74,7 @@ const rootProps = computed(() => {
 
 <template>
   <component
-    :is="tag"
+    :is="props.tag"
     v-bind="rootProps"
   >
     <!-- @slot Where all default content should be placed. -->
