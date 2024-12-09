e364d09a7285727341931c66dcea0267736d578a
David Sinclair <dsincla@rei.com>
2024-12-09 14:09:42 -0800

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3001/rei-cedar/',
+    url: 'http://localhost:3000/rei-cedar/',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
   },
