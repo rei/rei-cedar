0ca6814ce01eb8dc6b6833caa120a9e7643d2bfc
David Sinclair <dsincla@rei.com>
2024-12-27 15:43:18 -0800

    media-object-refinement

--- src/components/choreographer/CdrChoreographer.vue
+++ src/components/choreographer/CdrChoreographer.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import { useCssModule, type PropType } from 'vue';
-import { CdrAbstract, CdrKicker, CdrTitle, CdrImg, CdrRating, CdrLink, CdrCard } from '../../lib';
+import {
+  CdrAbstract,
+  CdrKicker,
+  CdrTitle,
+  CdrBody,
+  CdrImg,
+  CdrRating,
+  CdrLink,
+  CdrCard,
+  CdrSurface,
+  CdrMediaObject,
+  CdrLayout,
+  CdrPicture,
+  CdrButton,
+  CdrContainer,
+} from '../../lib';
 import { choreographerSchema } from '../../types/interfaces';
 
 /** Choreographer is in an experimental stage and should be considered unstable */
@@ -13,20 +28,25 @@ const componentMap = {
   abstract: CdrAbstract,
   kicker: CdrKicker,
   title: CdrTitle,
+  body: CdrBody,
   image: CdrImg,
+  picture: CdrPicture,
   rating: CdrRating,
   link: CdrLink,
   card: CdrCard,
+  surface: CdrSurface,
+  mediaObject: CdrMediaObject,
+  layout: CdrLayout,
+  button: CdrButton,
+  container: CdrContainer,
 };
 
 defineProps({
-  schema: { type: Array as PropType<choreographerSchema[]>, default: () => [] }
+  schema: { type: Array as PropType<choreographerSchema[]>, default: () => [] },
 });
 
 const baseClass = 'cdr-choreographer';
 const style = useCssModule();
-
-
 </script>
 
 <template>
@@ -38,7 +58,7 @@ const style = useCssModule();
     :class="style[`${baseClass}__${entry.type}`]"
   >
     <template v-if="Array.isArray(entry?.content)">
-      <CdrChoreographer :schema="(entry?.content)" />
+      <CdrChoreographer :schema="entry?.content" />
     </template>
     <template v-else>
       {{ entry?.content }}
@@ -46,5 +66,4 @@ const style = useCssModule();
   </component>
 </template>
 
-<style lang="scss" module src="./styles/CdrChoreographer.module.scss">
-</style>
+<style lang="scss" module src="./styles/CdrChoreographer.module.scss"></style>
