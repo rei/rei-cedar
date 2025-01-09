d1f42a58c97543759f029f7f5caab00d5e245123
David Sinclair <dsincla@rei.com>
2025-01-09 13:16:24 -0800

    media-object-refinement

--- src/components/choreographer/CdrChoreographer.vue
+++ src/components/choreographer/CdrChoreographer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useCssModule, computed } from 'vue';
+import { useCssModule, markRaw } from 'vue';
 import {
   CdrAbstract,
   CdrKicker,
@@ -30,24 +30,21 @@ const props = withDefaults(defineProps<Choreographer>(), {
   components: () => ({}),
 });
 
-const componentMap = computed(
-  () =>
-    ({
-      abstract: CdrAbstract,
-      kicker: CdrKicker,
-      title: CdrTitle,
-      body: CdrBody,
-      image: CdrImg,
-      picture: CdrPicture,
-      rating: CdrRating,
-      link: CdrLink,
-      card: CdrCard,
-      layout: CdrLayout,
-      button: CdrButton,
-      container: CdrContainer,
-      ...props.components,
-    }) as ChoreographerComponents,
-);
+const componentMap = markRaw({
+  abstract: CdrAbstract,
+  kicker: CdrKicker,
+  title: CdrTitle,
+  body: CdrBody,
+  image: CdrImg,
+  picture: CdrPicture,
+  rating: CdrRating,
+  link: CdrLink,
+  card: CdrCard,
+  layout: CdrLayout,
+  button: CdrButton,
+  container: CdrContainer,
+  ...props.components,
+}) as ChoreographerComponents;
 
 const baseClass = 'cdr-choreographer';
 const style = useCssModule();
