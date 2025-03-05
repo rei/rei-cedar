a9b1b36616c9faa0fa8786ecc3f21fd5bf8fb477
Kurt Medley <kmedley@rei.com>
2025-03-05 14:44:39 -0800

    Binding attrs to CdrScrollCarouselEngine so we can style the base carousel at the implementation level if we need to

---
+++ src/components/scrollCarousel/examples/Lifestyle/Example.vue
@@ -0,0 +1,45 @@
+<template>
+  <CdrScrollCarousel
+    class="lifestyle-scroll-carousel"
+    :model="lifestyleModelData"
+    :adapter="LifestyleAdapter"
+    @slide-click="lifestyleOnSlideClick"
+    @arrow-click="lifestyleOnArrowClick"
+    @resize="onResize"
+  />
+</template>
+
+<script setup lang="ts">
+import CdrScrollCarousel from '../../CdrScrollCarousel.vue';
+
+import type { LifestyleModel } from './index';
+
+import lifestyleModel from './mock.json';
+
+// Models
+const lifestyleModelData = lifestyleModel as LifestyleModel;
+
+// Handlers
+import {
+  onSlideClick as lifestyleOnSlideClick,
+  onArrowClick as lifestyleOnArrowClick,
+  onResize,
+} from './handlers';
+
+// Adapters
+import LifestyleAdapter from './adapter';
+</script>
+
+<style lang="scss" scoped>
+@import '@rei/cdr-tokens/dist/rei-dot-com/scss/cdr-tokens.scss';
+
+.lifestyle-scroll-carousel {
+  margin-left: -$cdr-space-one-x;
+  margin-right: -$cdr-space-one-x;
+
+  @include cdr-sm-mq-up {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
