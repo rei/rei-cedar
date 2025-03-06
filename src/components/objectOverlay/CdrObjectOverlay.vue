67e55a8c6fe8cc4ac55ec62604a7163f337d5472
Michael Hewson <mhewson@gmail.com>
2025-03-06 15:02:31 -0800

    just messing around with positions

--- src/components/objectOverlay/CdrObjectOverlay.vue
+++ src/components/objectOverlay/CdrObjectOverlay.vue
@@ -1,58 +1,179 @@
 <script setup lang="ts">
-import { useCssModule, computed } from 'vue';
+import { useCssModule, computed, watch } from 'vue';
 import mapClasses from '../../utils/mapClasses';
-
-/** Component for positioning content in 9 different positions relative to a container */
 
 defineOptions({ name: 'CdrObjectOverlay' });
 
+interface ResponsivePosition {
+  xs?: string;
+  sm?: string;
+  md?: string;
+  lg?: string;
+}
+
 interface ObjectOverlayProps {
-  /** Position of the content relative to the container */
-  position?:
-    | 'top-left'
-    | 'top-center'
-    | 'top-right'
-    | 'left'
-    | 'center'
-    | 'right'
-    | 'bottom-left'
-    | 'bottom-center'
-    | 'bottom-right';
-  /** Margin space around the positioned content */
+  position?: string | ResponsivePosition;
   margin?: string;
-  /** Sets the HTML tag for the container element */
+  marginTop?: string;
+  marginBottom?: string;
+  marginLeft?: string;
+  marginRight?: string;
+  padding?: string;
+  background?: string;
+  customClass?: string;
+  customStyle?: Record<string, string>;
+  onPositionChange?: (newPosition: string | ResponsivePosition) => void;
   tag?: string;
 }
 
 const props = withDefaults(defineProps<ObjectOverlayProps>(), {
   position: 'center',
   margin: '0',
+  marginTop: '0',
+  marginBottom: '0',
+  marginLeft: '0',
+  marginRight: '0',
+  padding: '0',
+  background: 'transparent',
+  customClass: '',
   tag: 'div',
 });
 
 const style = useCssModule();
 
-const positionClass = computed(() => `cdr-object-overlay--${props.position}`);
-const containerStyle = computed(() => ({
-  '--cdr-object-overlay-margin': props.margin,
+const positionClass = computed(() => {
+  if (typeof props.position === 'string') {
+    return props.position;
+  } else {
+    return Object.entries(props.position || {})
+      .map(([breakpoint, pos]) => `${breakpoint}-${pos}`)
+      .join(' ');
+  }
+});
+
+const dataAttributes = computed(() => ({
+  'data-position': positionClass.value,
+  style: {
+    '--cdr-object-overlay-component-margin': props.margin ?? '0',
+    '--cdr-object-overlay-component-margin-top': props.marginTop ?? '0',
+    '--cdr-object-overlay-component-margin-bottom': props.marginBottom ?? '0',
+    '--cdr-object-overlay-component-margin-left': props.marginLeft ?? '0',
+    '--cdr-object-overlay-component-margin-right': props.marginRight ?? '0',
+    '--cdr-object-overlay-component-padding': props.padding ?? '0',
+    '--cdr-object-overlay-component-background': props.background ?? 'transparent',
+  },
 }));
+
+if (props.onPositionChange) {
+  watch(() => props.position, (newPosition) => {
+    props.onPositionChange?.(newPosition);
+  });
+}
 </script>
 
 <template>
   <component
     :is="tag"
-    :class="mapClasses(style, 'cdr-object-overlay', positionClass)"
-    :style="containerStyle"
+    :class="mapClasses(style, 'cdr-object-overlay', props.customClass || '')"
+    v-bind="dataAttributes"
   >
     <div :class="style['cdr-object-overlay__container']">
-      <!-- @slot Container content that the overlay will be positioned relative to -->
       <slot name="container" />
     </div>
     <div :class="style['cdr-object-overlay__content']">
-      <!-- @slot Content to be positioned -->
       <slot name="content" />
     </div>
   </component>
 </template>
 
-<style lang="scss" module src="./styles/CdrObjectOverlay.module.scss" />
+<style lang="scss" module src="./styles/CdrObjectOverlay.module.scss">
+.cdr-object-overlay {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  --cdr-object-overlay-margin: 
+  var(--cdr-object-overlay-component-margin, var(--cdr-token-margin, 0));
+  --cdr-object-overlay-margin-top: 
+  var(--cdr-object-overlay-component-margin-top, var(--cdr-token-margin-top, 0));
+  --cdr-object-overlay-margin-bottom: 
+  var(--cdr-object-overlay-component-margin-bottom, var(--cdr-token-margin-bottom, 0));
+  --cdr-object-overlay-margin-left: 
+  var(--cdr-object-overlay-component-margin-left, var(--cdr-token-margin-left, 0));
+  --cdr-object-overlay-margin-right: 
+  var(--cdr-object-overlay-component-margin-right, var(--cdr-token-margin-right, 0));
+  --cdr-object-overlay-padding: 
+  var(--cdr-object-overlay-component-padding, var(--cdr-token-padding, 0));
+  --cdr-object-overlay-background: 
+  var(--cdr-object-overlay-component-background, var(--cdr-token-background, transparent));
+
+  &__container {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__content {
+    position: absolute;
+    margin: var(--cdr-object-overlay-margin);
+    margin-top: var(--cdr-object-overlay-margin-top);
+    margin-bottom: var(--cdr-object-overlay-margin-bottom);
+    margin-left: var(--cdr-object-overlay-margin-left);
+    margin-right: var(--cdr-object-overlay-margin-right);
+    padding: var(--cdr-object-overlay-padding);
+    background: var(--cdr-object-overlay-background);
+  }
+
+  // Top positions
+  &[data-position="top-left"] &__content {
+    top: 0;
+    left: 0;
+  }
+
+  &[data-position="top-center"] &__content {
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &[data-position="top-right"] &__content {
+    top: 0;
+    right: 0;
+  }
+
+  // Center positions
+  &[data-position="left"] &__content {
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+  }
+
+  &[data-position="center"] &__content {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &[data-position="right"] &__content {
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+  }
+
+  // Bottom positions
+  &[data-position="bottom-left"] &__content {
+    bottom: 0;
+    left: 0;
+  }
+
+  &[data-position="bottom-center"] &__content {
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &[data-position="bottom-right"] &__content {
+    bottom: 0;
+    right: 0;
+  }
+}
+</style>
--- src/components/objectOverlay/examples/ObjectOverlay.vue
+++ src/components/objectOverlay/examples/ObjectOverlay.vue
@@ -5,40 +5,71 @@ defineOptions({ name: 'ObjectOverlay' });
 </script>
 
 <template>
-  <CdrObjectOverlay position="top-right" margin="1rem">
-    <template #container>
-      <img src="https://placehold.co/600x400" width="100%" height="100%" alt="Background image" />
-    </template>
-    <template #content>
-      <div class="overlay-content" :style="{ backgroundColor: '#fff', padding: '20px' }">
-        Positioned Content
-      </div>
-    </template>
-  </CdrObjectOverlay>
+  <div>
+    <h2>Default Overlay</h2>
+    <CdrObjectOverlay>
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
 
-  <hr />
+    <h2>Custom Position</h2>
+    <CdrObjectOverlay position="top-right">
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
 
-  <CdrObjectOverlay position="center">
-    <template #container>
-      <img src="https://placehold.co/600x400" width="100%" height="100%" alt="Background image" />
-    </template>
-    <template #content>
-      <div class="overlay-content" :style="{ backgroundColor: '#fff', padding: '20px' }">
-        Positioned Content
-      </div>
-    </template>
-  </CdrObjectOverlay>
+    <h2>Responsive Position</h2>
+    <CdrObjectOverlay :position="{ xs: 'top-left', md: 'center', lg: 'bottom-right' }">
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
 
-  <hr />
+    <h2>Custom Margins and Padding</h2>
+    <CdrObjectOverlay margin="10px" padding="20px">
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
 
-  <CdrObjectOverlay position="bottom-left" margin="1rem">
-    <template #container>
-      <img src="https://placehold.co/600x400" width="100%" height="100%" alt="Background image" />
-    </template>
-    <template #content>
-      <div class="overlay-content" :style="{ backgroundColor: '#fff', padding: '20px' }">
-        Positioned Content
-      </div>
-    </template>
-  </CdrObjectOverlay>
+    <h2>Custom Background</h2>
+    <CdrObjectOverlay background="rgba(0, 0, 0, 0.5)">
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
+
+    <h2>Custom Class</h2>
+    <CdrObjectOverlay customClass="custom-overlay-class">
+      <template #container>
+        <div>Container Content</div>
+      </template>
+      <template #content>
+        <div>Overlay Content</div>
+      </template>
+    </CdrObjectOverlay>
+  </div>
 </template>
+
+<style scoped>
+.custom-overlay-class {
+  border: 2px solid red;
+}
+</style>
