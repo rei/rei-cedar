9f0a745f770339e94eeaef3dae278047fd726867
Alex Perkins <1110797+peripateticus@users.noreply.github.com>
2025-11-12 09:29:05 -0800

    Update browser target versions

--- browserTargets.mjs
+++ browserTargets.mjs
@@ -1,7 +1,7 @@
 export default [
-  'Chrome >= 111',
-  'Firefox >= 121',
-  'iOS >= 15.6',
-  'Safari >= 15.6',
-  'Edge >= 120',
+  'Chrome >= 135',
+  'Firefox >= 140',
+  'iOS >= 16.6',
+  'Safari >= 16.6',
+  'Edge >= 138',
 ];
